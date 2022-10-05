ca1fc219f517d34468b15af769ca56d568f85b23
KASHI TAMANG <tamang.kashi@gmail.com>
2022-10-04 17:43:37 -0700

    adding UserProvider, request service, auth service, and wrapping router in context provider

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,33 +15,38 @@ import VSssnake from './Projects/VSssnake/VSssnake';
 import Search from './Search/Search';
 import Auth from './Auth/Auth.js';
 import AuthForm from './Auth/AuthForm.js';
-
+import UserProvider from '../state/UserContext.js';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="auth" element={<Auth />}>
-          <Route index element={<AuthForm mode="signin" />} />
-          <Route path="signup" element={<AuthForm mode="signup" />} />
-        </Route>
-
-        <Route element={<Layout />}>
-          <Route index element={<HomePage />} />
-
-          <Route path="about" element={<About />} />
-
-          <Route path="search" element={<Search />} />
-
-          <Route path="projects" element={<Projects />}>
-            <Route index element={<BooBot />} />
-            <Route path="printstagram" element={<Printstagram />} />
-            <Route path="vsssnake" element={<VSssnake />} />
+      <UserProvider>
+        <Routes>
+          <Route path="auth" element={<Auth />}>
+            <Route index element={<AuthForm mode="signin" />} />
+            <Route
+              path="signup"
+              element={<AuthForm mode="signup" />}
+            />
           </Route>
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+          <Route element={<Layout />}>
+            <Route index element={<HomePage />} />
+
+            <Route path="about" element={<About />} />
+
+            <Route path="search" element={<Search />} />
+
+            <Route path="projects" element={<Projects />}>
+              <Route index element={<BooBot />} />
+              <Route path="printstagram" element={<Printstagram />} />
+              <Route path="vsssnake" element={<VSssnake />} />
+            </Route>
+
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </UserProvider>
     </Router>
   );
 }
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,11 +1,17 @@
-import { Outlet } from 'react-router-dom';
-// import styles from './Auth.css';
+import { Navigate, Outlet } from 'react-router-dom';
+import styles from './Auth.css';
+// import { useUser } from '../../state/UserContext.jsx';
 
 export default function Auth() {
+  // const user = useUser();
+
+  // TODO: handle clever redirect to page that got us here
+  // if (user) return <Navigate to="/" />;
+
   return (
-    <div>
-      Auth Form
-      <Outlet/>
-    </div>
+    <main className={styles.Auth}>
+      <h1>Shopping List</h1>
+      <Outlet />
+    </main>
   );
 }
